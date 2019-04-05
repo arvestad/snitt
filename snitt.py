a4f872ea8c3d3641d2716222d79690df62942329
Pär <par@wlan-su-10-201-9-155.local.su.se>
2019-04-05 16:43:23 +0200

    Added reverse order in filter_pairs

--- snitt.py
+++ snitt.py
@@ -28,6 +28,8 @@ def filter_pairs(h, known_pairs):
         a, b = line.split()
         if (a,b) in known_pairs:
             n_shared += 1
+        elif (b, a) in known_pairs:  # Added for the possibility that pairs are in reverse order.
+            n_shared += 1
         else:
             n_unique += 1
     return n_shared, n_unique
