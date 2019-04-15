2cc5cc2aea8555bf5e120e902cd7f6f813fb231a
JoelAhfeldt <49274731+JoelAhfeldt@users.noreply.github.com>
2019-04-15 16:35:23 +0200

    Update snitt.py

--- snitt.py
+++ snitt.py
@@ -28,7 +28,7 @@ def filter_pairs(h, known_pairs):
         a, b = line.split()
         if (a,b) in known_pairs:
             n_shared += 1
-        else if (b,a) in known_pairs: ##Kollar paren åt båda hållen.
+        elif (b,a) in known_pairs: ##Kollar paren åt båda hållen.
             n_shared += 1
         else:
             n_unique += 1
