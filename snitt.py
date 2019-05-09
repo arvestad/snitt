4f36b33f732e470a0b80cf3493c201a245cc8cd5
Adrian Axelsson <48991008+adax1387@users.noreply.github.com>
2019-05-09 08:52:33 +0200

    Update

--- snitt.py
+++ snitt.py
@@ -26,7 +26,7 @@ def filter_pairs(h, known_pairs):
     n_unique = 0
     for line in h:
         a, b = line.split()
-        if (a,b) in known_pairs:
+        if (a,b) in known_pairs or (b,a) in known_pairs:
             n_shared += 1
         else:
             n_unique += 1
@@ -45,8 +45,8 @@ def main(file1, file2):
         
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("file1")
-    parser.add_argument("file2")
+    args = parser.add_argument("file1")
+    args = parser.add_argument("file2")
 
     args=parser.parse_args()
 
