8a19a305041f40ac3e35bd1bfec37acdc04848f5
dama6758 <dama6758@student.su.se>
2023-03-30 10:32:43 +0200

    Suggestion for fixing bug

--- snitt.py
+++ snitt.py
@@ -19,14 +19,15 @@ def load_pairs(h):
 def filter_pairs(h, known_pairs):
     '''
     Read pairs (like in load_pairs) from the input file h and count how many of the pairs 
-    are shared with what is stored in the dictionary known_pairs. Also count how many
+    are shared with what is stored in the dictionary known_pairs, taking into account that 
+    eg (a b) is the same pair as (b a). Also count how many
     of the pairs in h are unique to that file. Return the number of shared and unique pairs.
     '''
     n_shared = 0
     n_unique = 0
     for line in h:
         a, b = line.split()
-        if (a,b) in known_pairs:
+        if (a,b) in known_pairs or (b,a) in known_pairs:
             n_shared += 1
         else:
             n_unique += 1
